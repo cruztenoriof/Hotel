9a1f70bba13507a5f6985c50ca0a42d46d5a5f42
Tu Nombre <cruztenoriof@gmail.com>
2026-07-25 08:25:36 -0600

    Entidad huespedes

---
+++ huespedes/src/main/java/com/francisco/huespedes/HuespedesApplication.java
@@ -0,0 +1,13 @@
+package com.francisco.huespedes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuespedesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuespedesApplication.class, args);
+	}
+
+}
